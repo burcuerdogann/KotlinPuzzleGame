43200653faa5be87d2c7ba90037515e750131088
Burcu Erdogan <designbyburcuerdogan@gmail.com>
2022-08-22 02:53:29 +0300

    Initial commit

---
+++ app/src/main/java/com/burcuerdogan/kotlinpuzzlegame/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.burcuerdogan.kotlinpuzzlegame
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        /* first we add some images in assets*/
+    }
+
+    fun onImageCameraClicked(view: View) {}
+}
---
+++ app/src/main/java/com/burcuerdogan/kotlinpuzzlegame/PuzzleActivity.kt
@@ -0,0 +1,11 @@
+package com.burcuerdogan.kotlinpuzzlegame
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PuzzleActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_puzzle)
+    }
+}
